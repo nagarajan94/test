facca4167c9bee4a8acccfa69a4fec9775394896
nagarajan94 <nagarajanece94@gmail.com>
2022-12-21 12:15:04 +0530

    test

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,34 @@
+package org.emp;
+
+public class Employee {
+public void empId() {
+	// TODO Auto-generated method stub
+System.out.println("my employee id :245");
+}
+public void empName() {
+	// TODO Auto-generated method stub
+	System.out.println("my employee name is nagarajan");
+
+}
+public void phnNum() {
+	// TODO Auto-generated method stub
+System.out.println("my phone number is 9655559446");
+}
+public void empMailId() {
+	// TODO Auto-generated method stub
+System.out.println("my mail id is nagarajanece94@gamil.com");
+}
+public void empAddress() {
+	// TODO Auto-generated method stub
+System.out.println("my address is singampunari");
+}
+public static void main(String[] args) {
+	Employee S=new Employee();
+	S.empId();
+	S.empName();
+	S.phnNum();
+	S.empMailId();
+	S.empAddress();
+	
+}
+}
